93b44ea793b1741c6e1bd8ef8b2791e2feb4bef9
“Rodrigo <rodrigowebx@gmail.com>
2022-07-31 16:16:19 -0300

    Implementação das rotas na tela Home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './home.css'
 import { Header } from "../../components/header/header";
+import { Link } from 'react-router-dom';
 
 
 export const Home = () => {
@@ -11,7 +12,9 @@ export const Home = () => {
                 <div id='card' className="col-lg-6 col-md-8 mx-auto bg-light p-4 rounded">
                     <h1 className="text-center">Nos comprometemos com <p className="text-center text-success">SUA ECONOMIA</p></h1> <br />
                     <p className="lead text-muted">Uma nova forma de você ter maior controle de uma das finanças mais problemáticas da sociedade: Conta de energia. Nosso objetivo é lhe auxiliar, para que você possa ter mais tempo para fazer o que gosta! Bem vindo ao <strong>BF ENERGY!</strong></p> <br />
-                    <a href="login" className="d-flex justify-content-center text-decoration-none"><button type="button" className="btn btn-outline-success">Veja mais!</button></a>    
+                    <div className='d-flex justify-content-center'>
+                        <a><Link className='btn btn-primary'to='/sobre'>Veja mais!</Link></a>
+                    </div>    
                 </div>
             </section>
         </div>
