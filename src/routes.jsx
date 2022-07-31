30f0b60a20276d97c3c70523a60bca9b573b6dce
“Rodrigo <rodrigowebx@gmail.com>
2022-07-31 12:09:05 -0300

    Implementação da página Informacoes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Informacoes } from './pages/informacoes/informacoes';
 
 export const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                
+                <Route path='/informacoes' exect component={Informacoes}/>
             </Switch>
         </BrowserRouter>
     )
