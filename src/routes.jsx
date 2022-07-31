697dcfd0059c5419ba5571110c1687ba2a9da873
“Rodrigo <rodrigowebx@gmail.com>
2022-07-31 15:03:47 -0300

    Implementação da primeira MVP da tela Sobre

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Sobre } from './pages/sobre/sobre';
 
 export const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                
+                <Route path='/sobre' exact component={Sobre}/>
             </Switch>
         </BrowserRouter>
     )
