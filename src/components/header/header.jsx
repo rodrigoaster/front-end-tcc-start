e258d9e952c441537034704b2aa38846489cab7b
“Rodrigo <rodrigowebx@gmail.com>
2022-07-31 15:29:28 -0300

    Implementação da tela InfoEquipe

---
+++ src/components/header/header.jsx
@@ -0,0 +1,29 @@
+export const Header = () => {
+    return (
+        <nav className="navbar px-5 bg-light">
+            <div className="container-fluid">
+                <a  href="perfil">
+                    <img src="src/assets/img/logo/logo.png" alt="" width="90" height="90" className="d-inline-block align-text-top"/>
+                    
+                </a>
+                <ul className="nav justify-content-end nav-pills">
+                    <li className="nav-item">
+                        <a className="nav-link" aria-current="page" href="/">HOME</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="sobre">SOBRE</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="informacoes">INFORMAÇÕES</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="calculadora">CÁLCULO</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="equipe">CONTATO</a>
+                    </li>                   
+                </ul>
+            </div>
+        </nav>
+    )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { InfoEquipe } from './pages/infoEquipe/infoEquipe';
 
 export const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                
+                <Route path='/equipe' exact component={InfoEquipe}/>
             </Switch>
         </BrowserRouter>
     )
