ccb71c41dfc06ae8ba78404044522bcf4ead5767
“Rodrigo <rodrigowebx@gmail.com>
2022-07-31 23:44:24 -0300

    Ajustes no footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export const Footer = () => {
     return (
         <div className="d-flex justify-content-evenly bg-dark p-3  align-items-center">
-            <p className="text-light">Desenvolvido por Sirius</p>
+            <p className="text-light mt-4">Desenvolvido por Sirius</p>
             <Link to='/equipe' className="btn btn-primary">Entre em contato!</Link>
         </div>
     )
