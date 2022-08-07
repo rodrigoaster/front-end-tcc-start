891c0bc87e03acfd7dd5c6a2bc4702ce393cba5c
Joyce Almeida <78738510+JoyAlmDev@users.noreply.github.com>
2022-08-07 16:35:16 -0300

    Alteração na altura e largura da logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
         <nav className="navbar px-5 bg-light">
             <div className="container-fluid">
                 <a  href="perfil">
-                    <img src="src/assets/img/logo/logo.png" alt="" width="90" height="90" className="d-inline-block align-text-top"/>
+                    <img src="src/assets/img/logo/logo.png" alt="" width="100" height="100" className="d-inline-block align-text-top"/>
                 </a>
                 <ul className="nav justify-content-end nav-pills">
                     <li className="nav-item">
