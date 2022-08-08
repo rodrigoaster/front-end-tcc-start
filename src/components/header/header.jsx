b4e7daf59c36f806348bf4ab3b3066f9c20167c8
Allan Souza <allanzissimo@gmail.com>
2022-08-08 14:24:53 -0300

    implementação dos links da navbar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,11 +5,26 @@ export const Header = () => {
         <nav className="navbar px-5 bg-light">
             <div className="container-fluid">
                 <a  href="perfil">
-                    <img src="src/assets/img/logo/logo.png" alt="" width="90" height="90" className="d-inline-block align-text-top"/>
+                    <img src="src/assets/img/logo/logo.png" alt="" width="120" height="120" className="d-inline-block align-text-top"/>
                     
                 </a>
                 <ul className="nav justify-content-end nav-pills">
+                <li className="nav-item">
+                        <Link className="nav-link" to='/'>HOME</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to='/sobre'>SOBRE</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to='/informacoes'>INFORMAÇÕES</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to='/calculo'>CÁLCULO</Link>
+                    </li>
+                    <li className="nav-item">
 
+                        <Link className="nav-link active" to='/equipe'>CONTATO</Link>
+                    </li>                
                 </ul>
             </div>
         </nav>
