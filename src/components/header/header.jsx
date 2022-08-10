c67915c84b4a87b2f44668d6bd7d82c7e43becaf
Allan Souza <allanzissimo@gmail.com>
2022-08-10 19:37:34 -0300

    responsividade do header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,33 +1,39 @@
 import { Link } from "react-router-dom"
+import './header.css'
 
 export const Header = () => {
     return (
-        <nav className="navbar px-5 bg-light">
-            <div className="container-fluid">
-                <a  href="perfil">
-                    <img src="src/assets/img/logo/logo.png" alt="" width="90" height="90" className="d-inline-block align-text-top"/>
-                </a>
-                <ul className="nav justify-content-end nav-pills">
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/'>HOME</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/sobre'>SOBRE</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/informacoes'>INFORMAÇÕES</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/calculo'>CÁLCULO</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/equipe'>CONTATO</Link>
-                    </li>                   
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/login'>LOGIN</Link>
-                    </li>                   
-                </ul>
-            </div>
-        </nav>
+        <nav className="navbar bg-light navbar-expand-lg">
+      
+        <div className="container-fluid">
+            <a  href="perfil">
+                <img src="src/assets/img/logo/logo.png" alt="" width="90" height="90" className="d-inline-block align-text-top"/>              
+            </a>
+  
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-expanded="false" aria-controls="navbarNav" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+  
+          <div id="navbarNav" className="collapse navbar-collapse nav nav-pills">
+            <ul className="navbar-nav ">
+                <li className="nav-item">
+                    <Link className="nav-link" to='/'>Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to='/sobre'>Sobre</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to='/informacoes'>Informações</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to='/calculo'>Cálculo</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to='/equipe'>Contato</Link>
+                </li>                   
+            </ul>
+          </div>         
+        </div>
+    </nav>
     )
 }
