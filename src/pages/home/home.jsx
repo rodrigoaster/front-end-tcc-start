5d9ea6da4ad3feb6882ef26ab4ce74d89ec2e0d0
Rodrigo Aster <rodrigowebx@gmail.com>
2022-08-15 01:17:17 -0300

    Atualização page Home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,23 +1,24 @@
 import { Header } from "../../components/header/header";
 import { Link } from 'react-router-dom';
 import { Footer } from '../../components/footer/footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './home.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const Home = () => {
     return (
-        <div>
+        <div className="">
             <Header/>
-            <section className="background-default pt-5">
-                <div id='card' className="col-lg-6 col-md-8 mx-auto bg-light p-4 rounded">
-                    <h1 className="text-center">Nos comprometemos com <p className="text-center text-success">SUA ECONOMIA</p></h1> <br />
-                    <p className="lead text-muted">Uma nova forma de você ter maior controle de uma das finanças mais problemáticas da sociedade: Conta de energia. Nosso objetivo é lhe auxiliar, para que você possa ter mais tempo para fazer o que gosta! Bem vindo ao <strong>BF ENERGY!</strong></p> <br />
-                    <div className='d-flex justify-content-center'>
-                        <a><Link className='btn btn-primary'to='/sobre'>Veja mais!</Link></a>
-                    </div>    
+            <div className="d-flex bg justify-content-center">
+                    <div className="w-50 bg-light p-4 rounded">
+                        <h1 className="text-center">Nos comprometemos com <p className="text-center text-success">SUA ECONOMIA</p></h1> <br />
+                        <p className="lead text-muted">Uma nova forma de você ter maior controle de uma das finanças mais problemáticas da sociedade: Conta de energia. Nosso objetivo é lhe auxiliar, para que você possa ter mais tempo para fazer o que gosta! Bem vindo ao <strong>BF ENERGY!</strong></p> <br />
+                        <div className='d-flex justify-content-center'>
+                            <a><Link className='btn btn-primary'to='/sobre'>Veja mais!</Link></a>
+                        </div>
+                    </div>
                 </div>
-            </section>
-            <Footer/>
+                <Footer/>
         </div>
     )
 }
