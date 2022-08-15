45aae1687e6241941a94f1f62729f503df775a6f
Rodrigo Aster <rodrigowebx@gmail.com>
2022-08-15 01:23:28 -0300

    Adicionando estilização extra no component Footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom"
+import './footer.css'
 
 export const Footer = () => {
     return (
-        <div className="d-flex justify-content-evenly bg-dark p-3  align-items-center">
-            <p className="text-light mt-4">Desenvolvido por Sirius</p>
-            <Link to='/equipe' className="btn btn-primary">Entre em contato!</Link>
+        <div className="d-flex footer">
+            <div className="d-flex justify-content-evenly bg-dark p-3  align-items-center w-100">
+                <p className="text-light mt-4">Desenvolvido por Sirius</p>
+                <Link to='/equipe' className="btn btn-primary">Entre em contato!</Link>
+            </div>
         </div>
     )
 }
