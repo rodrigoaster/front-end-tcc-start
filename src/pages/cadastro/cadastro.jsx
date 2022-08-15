24676a533d6ac88ae3c7f66cf5cd65c44e23e25b
Rodrigo Aster <rodrigowebx@gmail.com>
2022-08-15 01:12:53 -0300

    Atualização da page de cadastro

--- src/pages/cadastro/cadastro.jsx
+++ src/pages/cadastro/cadastro.jsx
@@ -1,48 +1,100 @@
-import {Header} from '../../components/header/header'
+import { Header } from '../../components/header/header'
 import { Link } from 'react-router-dom';
 import { Footer } from '../../components/footer/footer';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { toast, ToastContainer } from 'react-toastify';
+import { Context } from '../../context/auth/UserContext';
+
+import * as yup from 'yup';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './cadastro.css'
+import 'react-toastify/dist/ReactToastify.min.css'
+import { useContext } from 'react';
+
 
 export const Cadastro = () => {
-    return(
+
+    const { createUser } = useContext(Context)
+
+    const validationRegister = yup.object().shape({
+        name: yup.string().min(16, 'Este campo deve conter no mínimo 16 caractéres').required('Este campo é obrigatório!'),
+
+        cpf: yup.string().max(11, 'Este campo deve conter 11 caractéres!').required('Este campo é obrigatório!'),
+
+        email: yup.string().email('Email inválido, tente novamente.').required('Este campo é obrigatório!'),
+
+        pass: yup.string().required('Este campo é obrigatório!'),
+
+        confirmpass: yup.string().oneOf([yup.ref("pass"), null], "As senhas não estão iguais!")
+    })
+
+
+    const handleClickRegister = async (user) => {
+        createUser(user)
+    }
+
+
+    return (
         <div>
             <Header/>
-            <main className="bg-def pt-5 padd-all">
-                <div class='container bg-light p-5 rounded shadow p-3 mb-5 mt-5 bg-body rounded'>
-                    <h1 className="text-center">CADASTRO</h1>
-                    <form class="input-group mb-3 d-flex" action="">
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Nome completo"/>
-                        </div>
+            <main class="bg-def pt-5 pb-5">
+                <div class="extra-pt ">
+                        <div class='container bg-light p-5 w-50 rounded mb-5'>
+                        <h1 class="text-center">Cadastro</h1>
+                            <Formik 
+                            initialValues={{}}
+                            onSubmit={handleClickRegister}
+                            validationSchema={validationRegister}
+                            >
+                            <Form>
+                                <div class="mb-3">
+                                    <Field name="name" class="form-control" placeholder="Nome completo"/>
 
-                        <div class="input-group mb-3">
-                            <input type="email" class="form-control" placeholder="Email"/>
-                            <span class="input-group-text">@</span>
-                        </div>
-                        
-                        <div class="input-group mb-3">
-                            <input type="number" class="form-control" placeholder="CPF"/>
-                        </div>
+                                    <ErrorMessage component="span" name="name" class="form-error"/>
+                                </div>
 
-                        <div class="input-group mb-3">
-                            <input type="password" class="form-control" placeholder="Senha"/>
-                        </div>
+                                <div class="mb-3">
+                                    <Field name="cpf" class="form-control" placeholder="CPF"/>
 
-                        <div class="input-group mb-3">
-                            <input type="password" class="form-control" placeholder="Confirmar senha"/>
-                        </div>
-                    </form>
-                    <div className="d-flex justify-content-center mt-2">
-                        <Link className="text-deco-none btn btn-success mt-2" to='/esqueciSenha'>Cadastrar</Link>  
-                    </div> 
-                    <div className="d-flex justify-content-center mt-5">
-                        <p className="extra-mr-5">Já possui conta?</p> 
-                        <Link className="text-deco-none " to='/login'>Conecte-se!</Link>  
-                    </div> 
-                </div>
+                                    <ErrorMessage component="span" name="cpf" class="form-error"/>
+                                </div>
+
+                                <div class="mb-3">
+                                    <Field name="email" className="form-control" placeholder="Email"/>
+
+                                    <ErrorMessage component="span" name="email" class="form-error"/>
+                                </div>
+
+                                <div class="mb-3">
+                                    <Field name="pass" class="form-control" type="password"  placeholder="Senha"/>
+
+                                    <ErrorMessage component="span" name="pass" class="form-error"/>
+                                </div>
+
+                                <div class="mb-3">
+                                    <Field name="confirmpass" type="password"  class="form-control" placeholder="Confirme a senha" />
+
+                                    <ErrorMessage component="span" name="confirmpass" class="form-error"/>
+                                </div>
+
+
+                                <div class="d-flex justify-content-center mt-5"><button type="submit" class="btn btn-success">Cadastrar</button></div>
+                            </Form>
+                            </Formik>
+                        <div class="btn-area d-flex justify-content-center mt-5">
+                            <p class="extra-mr-5">Já possui conta?</p> 
+                            <Link id="signUpBtn" class="text-deco-none " to='/login'>Conecte-se</Link>  
+                        </div> 
+                        <div class="d-flex justify-content-center mt-2">
+                            <Link class="text-deco-none btn btn-danger" to='/esqueciSenha'>Esqueci minha senha</Link>  
+                        </div> 
+                    </div>
+                </div> 
             </main>
+            <ToastContainer/>
             <Footer/>
         </div>
     )
 }
+
