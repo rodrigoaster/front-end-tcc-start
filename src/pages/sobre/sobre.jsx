6b9db82f594d42b09a98e93067bf137b4151e224
Rodrigo Aster <rodrigowebx@gmail.com>
2022-08-18 19:30:22 -0300

    Finalização da estilização da tela sobre

--- src/pages/sobre/sobre.jsx
+++ src/pages/sobre/sobre.jsx
@@ -8,7 +8,7 @@ export const Sobre = () => {
         <section>
             <Header/>
 
-            <main id="bg1" className="container border-bottom border-top">
+            <main id="bg1" className="container border-bottom border-top border-secondary ">
                 <h1 className="extra-mg-t">Você sabia que a conta de energia do Brasil <strong>é a segunda mais cara do mundo?</strong></h1><br/>
                 <div className="text-start">
                     <div className="container d-flex justify-content-between pt-3" >
@@ -21,10 +21,10 @@ export const Sobre = () => {
                 </div>
             </main>
 
-            <main id="bg2" className="extra-mg-t container justify-content-between d-flex align-items-center border-bottom">
+            <main id="bg2" className="extra-mg-t container justify-content-between d-flex align-items-center border-bottom  border-secondary">
                 <div className="container">
                     <h1>O que é o BF ENERGY?</h1><br/>
-                    <p class="extra-mg-b">Quem nunca se preocupou com quando chegou a conta de energia em casa, quantos planos foram deixados de lado para dar prioridade a ela foi pensando nisso que o BF ENERGY. Esse projeto veio com o objetivo de ajudar pessoas a diminuirem uma preocupação que normalmente afeta boa parte dos brasileiros, em especial aqueles que vivem no Nordeste,  <strong>CONTA DE ENERGIA.</strong> Buscamos fazer você ler, entender e como economizar mais nos gastos causados por essa finança:</p>
+                    <p class="extra-mg-b extra-mg-t fs-5">Quem nunca se preocupou com quando chegou a conta de energia em casa, quantos planos foram deixados de lado para dar prioridade a ela foi pensando nisso que o BF ENERGY. Esse projeto veio com o objetivo de ajudar pessoas a diminuirem uma preocupação que normalmente afeta boa parte dos brasileiros, em especial aqueles que vivem no Nordeste,  <strong>CONTA DE ENERGIA.</strong> Buscamos fazer você ler, entender e como economizar mais nos gastos causados por essa finança:</p>
                 </div>
 
                 <div class="img-bf">
@@ -50,11 +50,11 @@ export const Sobre = () => {
                 </div>
             </main>
 
-            <main id="bg4" className="container text-start height-default extra-pt padd-y">
+            <main id="bg4" className="container text-start height-default extra-pt padd-y border-top  border-secondary">
                 <h1>Seu próprio PERFIL</h1><br/>
                 <div className="container mt-5 d-flex justify-content-between" >
                     <div className="mt-5 mr-5 pt-5">
-                        <p className="extra-container-text-2 nav-font-size">Tenha seu próprio PERFIL, o qual você poderá armazenar todos os <strong>registros</strong> que fizer utilizando nossa ferramente, caso queira consultar e alterar dados para <strong>novos registros!</strong></p>
+                        <p className="extra-container-text-2 nav-font-size fs-5">Tenha seu próprio PERFIL, o qual você poderá armazenar todos os <strong>registros</strong> que fizer utilizando nossa ferramente, caso queira consultar e alterar dados para <strong>novos registros!</strong></p>
                     </div>
                     <img src="src/assets/img/sobre/tableex.png" style={{width: '600px'}} />
                     {/* <img id="img2-bg4" src="src/assets/img/sobre/people.png" style={{width: '400px', height:'437px'}} /> */}
